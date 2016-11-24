98560c2266cb2380578ff76aa6507cdbcd869891
Shadi Akiki <shadiakiki1986@gmail.com>
2016-11-24 16:54:32 +0200

    add config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
-mdbf0="/mnt/fingerprints" # windows share mount ... should match with /etc/fstab
-mdbf2="${mdbf0}/Program Files/ZKTeco/att2000.mdb" # /AccessControl
+# windows share mount ... should match with /etc/fstab
+mdbf2="/mnt/fingerprints/Program Files/ZKTeco/att2000.mdb"
 fpetc="/home/shadi/.ffa-mfe" # workspace folder
 mdbf="${fpetc}/att2000.mdb"
 df="${fpetc}/fingerprints-adapter-lastupdate.txt"
 lockfile="${fpetc}/fingerprints-lock.txt"
 
-instdir="/home/shadi/Development/ffa-mfe/fingerprints-adapter"
+instdir="/home/shadi/Development/ffa-zkteco-mfbf"
 
 mysqlUser=ffaFingerprints
 mysqlPass=fp1298
