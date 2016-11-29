c9f67533051882a6c79cbff8e16f2d42f3aad079
minerva22 <minerva.moawad@gmail.com>
2016-11-29 14:05:57 +0200

    Change checktype variable in line 45

--- lib/update_MF_USERS_LOCK.sql
+++ lib/update_MF_USERS_LOCK.sql
@@ -42,7 +42,7 @@ from (
 	) a on a.pin=acc_monitor_log.pin and a.id=acc_monitor_log.time -- id / CHECKTIME
 	union -- add absent people ... this is very slow
 	(
-		select USERINFO.USERID,'O' as CHECKTYPE,'1990-01-01' as id
+		select USERINFO.USERID,'1' as CHECKTYPE,'1990-01-01' as id
 		from USERINFO
 		where USERINFO.USERID not in (
 			SELECT distinct pin
