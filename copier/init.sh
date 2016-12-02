1d70232ad3fb37541e3556820b93ba698b7525d2
Shadi Akiki <shadiakiki1986@gmail.com>
2016-12-02 11:02:26 +0200

    copier: split out docker CMD to separate script and added if file exists for apache sites-enabled config file

---
+++ copier/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp /etc/ffa-zkteco-mfbf-copier-odbc/odbc.ini /etc/
+cp /etc/ffa-zkteco-mfbf-copier-odbc/odbcinst.ini /etc/
+envsubst < ./etc/apache2/copier.conf > /etc/apache2/sites-available/copier.conf
+if [ ! -f /etc/apache2/sites-enabled/copier.conf ]; then
+  ln -s /etc/apache2/sites-available/copier.conf /etc/apache2/sites-enabled/copier.conf
+fi
+service apache2 start
+php bin/natsListener.php
